e55ca5dfca1c24299d66b3bedb66090d305dc283
itsdeepan <itsdeepan@itsdeepsMacBook.default>
2016-05-04 19:24:43 +0100

    modified src file

--- src/test/java/test/org/seleniumhq/VerifyDownloadPageTest.java
+++ src/test/java/test/org/seleniumhq/VerifyDownloadPageTest.java
@@ -5,6 +5,8 @@ package test.org.seleniumhq;
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.WebDriver;
 	import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 	public class VerifyDownloadPageTest {
 		
@@ -13,6 +15,8 @@ package test.org.seleniumhq;
 			
 			WebDriver driver = new FirefoxDriver();
 			driver.get("http://www.seleniumhq.org/");
+			WebDriverWait wait = new WebDriverWait(driver,30);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Get Selenium']")));
 			driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
 			System.out.println(driver.getTitle());
 			Assert.assertEquals("Downloads", driver.getTitle());
