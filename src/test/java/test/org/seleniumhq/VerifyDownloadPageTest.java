e38042e6398426a7520a0b1d2d327123ce84023a
itsdeepan <itsdeepan@itsdeepsMacBook.default>
2016-05-04 19:29:29 +0100

    modified src file

--- src/test/java/test/org/seleniumhq/VerifyDownloadPageTest.java
+++ src/test/java/test/org/seleniumhq/VerifyDownloadPageTest.java
@@ -1,10 +1,13 @@
 package test.org.seleniumhq;
 	
-	import org.junit.Assert;
+	import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
 	import org.junit.Test;
 	import org.openqa.selenium.By;
 	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,8 +18,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 			
 			WebDriver driver = new FirefoxDriver();
 			driver.get("http://www.seleniumhq.org/");
-			WebDriverWait wait = new WebDriverWait(driver,30);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Get Selenium']")));
+			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 			driver.findElement(By.xpath("//a[@title='Get Selenium']")).click();
 			System.out.println(driver.getTitle());
 			Assert.assertEquals("Downloads", driver.getTitle());
